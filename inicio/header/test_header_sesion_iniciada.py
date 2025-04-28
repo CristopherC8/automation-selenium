c5aa3c85f066daca4574f2e2e89a404e3bae7f34
Cristopher Castillo <c.alejandro.castillo16@gmail.com>
2025-04-28 10:22:09 -0600

    Add test header_sesion_iniciada

--- inicio/header/test_header_sesion_iniciada.py
+++ inicio/header/test_header_sesion_iniciada.py
@@ -16,6 +16,12 @@ class TestHeader(unittest.TestCase):
         cls.driver.maximize_window()
         cls.driver.get("https://ef-storefront-web.enviaflores.com/")
 
+    def verificar_header_elements(self):
+         # Esperar a que el encabezado esté presente en el DOM
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@id='__nuxt']/div[2]/div/header/div"))
+        )
+
 
     def test_01_iniciar_sesion(self):
         # Esperar a que el botón de perfil sea clickeable y hacer clic
@@ -46,15 +52,20 @@ class TestHeader(unittest.TestCase):
         boton_enviar.click()
 
     def test_02_header_elements(self):
-        # Esperar a que el encabezado esté presente en el DOM
-        header = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[@id='__nuxt']/div[2]/div/header/div"))
-        )
-        time.sleep(3)
+        # # Esperar a que el encabezado esté presente en el DOM
+        # header = WebDriverWait(self.driver, 10).until(
+        #     EC.presence_of_element_located((By.XPATH, "//*[@id='__nuxt']/div[2]/div/header/div"))
+        # )
+        # time.sleep(3)
+        self.verificar_header_elements()
         # Screenshot del encabezado
-        header.screenshot("screenshots/header_sesion_iniciada/header.png")
+        self.driver.save_screenshot("screenshots/header_sesion_iniciada/header.png")
 
     def test_03_buscar(self):
+        # Esperar a que el overlay desaparezca
+        WebDriverWait(self.driver, 10).until(
+        EC.invisibility_of_element_located((By.CLASS_NAME, "v-overlay__scrim"))
+        )
         # Esperar a que el botón de buscar sea clickeable y hacer clic
         boton_buscar = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//*[@id='__nuxt']/div[2]/div/header/div/div[2]/div/div/div"))
@@ -65,6 +76,11 @@ class TestHeader(unittest.TestCase):
         boton_buscar.screenshot("screenshots/header_sesion_iniciada/buscar.png")
 
     def test_04_idioma_moneda(self):
+        # Esperar a que el overlay desaparezca
+        WebDriverWait(self.driver, 10).until(
+        EC.invisibility_of_element_located((By.CLASS_NAME, "v-overlay__scrim"))
+        )
+        
         # Esperar a que el botón de idioma y moneda sea clickeable y hacer clic
         boton_idioma_moneda = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//*[@id='__nuxt']/div[2]/div/header/div/div[2]/button[1]"))
@@ -117,6 +133,38 @@ class TestHeader(unittest.TestCase):
             EC.element_to_be_clickable((By.XPATH, "//*[@id='v-menu-33']/div/div/div[1]/button/span[3]/i"))
         )
         boton_cerrar.click()
+
+    def test_07_seleccionar_idioma(self):
+        # Esperar a que el botón de idioma sea clickeable y hacer clic
+        boton_idioma_moneda = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='__nuxt']/div[2]/div/header/div/div[2]/button[1]"))
+        )
+        boton_idioma_moneda.click()
+
+        # Esperar a que se muestre el menú de idioma
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//*[@id='v-menu-1']/div"))
+        )
+
+        # Hacer clic en English
+        boton_english = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='v-menu-1']/div/div/div[2]/div[2]/div/div/div[2]"))
+        )
+        boton_english.click()
+
+        # Hacer clic en el botón de aplicar
+        boton_aplicar = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='v-menu-1']/div/div/div[2]/button"))
+        )
+        boton_aplicar.click()
+        
+        # Llamar a la función para verificar los elementos del encabezado
+        self.verificar_header_elements()
+        
+        # Screenshot del encabezado después de cambiar el idioma
+        time.sleep(1)
+        self.driver.save_screenshot("screenshots/header_sesion_iniciada/header_ingles.png")
+
    
     @classmethod
     def tearDownClass(cls):
